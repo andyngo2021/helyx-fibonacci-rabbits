2e94b5151cef06a1f28b886566af6890bfa26caf
Andy Ngo <andyngo2021@gmail.com>
2020-12-26 19:49:54 -0800

    timer

---
+++ global_vars.js
@@ -0,0 +1,5 @@
+let c = [];
+let max_hours = 5;
+let start_time = 0;
+let bunny_life_span = 0
+let w, h;
--- script.js
+++ script.js
@@ -1,25 +1,32 @@
-let c = [];
-let max_hours = 30;
-let start_time = 0;
-let w, h;
+
 
 function setup()
 {
+    
     w = windowWidth;
     h = windowHeight*.80;
     createCanvas(w, h);
 
     for (let i=0; i<1; i++)
     {
-        c.push(new Rabbit(random(0, w), random(0, h), 20))
+        c.push(new Rabbit(random(0, w), random(0, h), bunny_life_span))
     }
 }
 
 function draw()
 {
     background(51); 
-    for (let i=0; i<1; i++)
+    for (let i=0; i<c.length; i++)
     {
         c[i].move()
     }
+
+
+
+    // time stuff
+    if (frameCount % 60 == 0) start_time++;
+    // putting text in js file for now to display time -> prob should move it to html file later
+    fill(255);
+    textSize(100);
+    text(start_time.toString(), 40, 100);
 }
